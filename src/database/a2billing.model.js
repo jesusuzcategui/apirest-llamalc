d68973212a61345d8d134b492280a4d9e4abe72f
JESUS UZCATEGUI <jesusu@titamedia.com>
2022-01-20 19:00:05 -0500

    Fix db conecction

--- src/database/a2billing.model.js
+++ src/database/a2billing.model.js
@@ -1,20 +1,22 @@
 const conx = require('./connection');
 const a2billing = () => {
-    this.updateUser = (idClient, ammout, result) => {
-        conx.query("UPDATE cc_card SET credit = ? WHERE id = ?", [ammout, idClient], (err, res) => {
-            if(err){
-                result(null, err);
-                return;
-            }
+    
+};
 
-            if(res.affectedRows === 0){
-                result({error: "No se ha actualizado la db"},null);
-                return;
-            }
+a2billing.updateUser = (idClient, ammout, result) => {
+    conx.query("UPDATE cc_card SET credit = ? WHERE id = ?", [ammout, idClient], (err, res) => {
+        if(err){
+            result(null, err);
+            return;
+        }
 
-            result(null, res);
-        });
-    };
+        if(res.affectedRows === 0){
+            result({error: "No se ha actualizado la db"},null);
+            return;
+        }
+
+        result(null, res);
+    });
 };
 
 module.exports = a2billing;
